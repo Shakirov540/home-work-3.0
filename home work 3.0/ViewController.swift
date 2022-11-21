9f8091a4f3c18c2bdef90be1f1e0296f2014476f
Aibek <ashakirov981@gmail.com>
2022-11-21 19:09:40 +0600

    Initial Commit

---
+++ home work 3.0/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  home work 3.0
+//
+//  Created by Айбек Шакиров on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var avatar: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        avatar.layer.cornerRadius = 25
+    }
+
+
+}
+
